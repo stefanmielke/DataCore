8f6ea3ca35fc868d45332aab3153dbfde1b78cc8
Stefan Mielke <stefanmielke@fastmail.com>
2017-01-19 20:26:16 -0200

    SelectById support. Closes #58

---
+++ DataCore.Test/DatabaseSelectTest.cs
@@ -0,0 +1,114 @@
+﻿using DataCore.Test.Models;
+using NUnit.Framework;
+
+namespace DataCore.Test
+{
+    [TestFixture]
+    public class DatabaseSelectTest
+    {
+        [Test, TestCaseSource(typeof(SqlTestDataFactory), nameof(SqlTestDataFactory.TestCases))]
+        public void CanSelect(TestHelper.DatabaseType dbType, string connectionString)
+        {
+            using (var connection = TestHelper.GetConnectionFor(dbType, connectionString))
+            {
+                var database = TestHelper.GetDatabaseFor(dbType, connection);
+
+                database.CreateTableIfNotExists<TestClass>();
+
+                var query = database.From<TestClass>().Where(t => t.Id == 1);
+
+                database.Select(query);
+
+                connection.Close();
+            }
+        }
+
+        [Test, TestCaseSource(typeof(SqlTestDataFactory), nameof(SqlTestDataFactory.TestCases))]
+        public void CanSelectSingle(TestHelper.DatabaseType dbType, string connectionString)
+        {
+            using (var connection = TestHelper.GetConnectionFor(dbType, connectionString))
+            {
+                var database = TestHelper.GetDatabaseFor(dbType, connection);
+
+                database.CreateTableIfNotExists<TestClass>();
+                database.Insert(TestHelper.GetNewTestClass());
+
+                var query = database.From<TestClass>().Where(t => t.Id == 1);
+
+                Assert.IsNotNull(database.SelectSingle(query));
+
+                connection.Close();
+            }
+        }
+
+        [Test, TestCaseSource(typeof(SqlTestDataFactory), nameof(SqlTestDataFactory.TestCases))]
+        public void CanPaginateSelect(TestHelper.DatabaseType dbType, string connectionString)
+        {
+            using (var connection = TestHelper.GetConnectionFor(dbType, connectionString))
+            {
+                var database = TestHelper.GetDatabaseFor(dbType, connection);
+
+                database.CreateTableIfNotExists<TestClass>();
+
+                var query = database.From<TestClass>().Where(t => t.Id > 1).OrderBy(t => t.Id).Paginate(10, 2);
+
+                database.Select(query);
+
+                connection.Close();
+            }
+        }
+
+        [Test, TestCaseSource(typeof(SqlTestDataFactory), nameof(SqlTestDataFactory.TestCases))]
+        public void ExistsReturnTrueWhenExists(TestHelper.DatabaseType dbType, string connectionString)
+        {
+            using (var connection = TestHelper.GetConnectionFor(dbType, connectionString))
+            {
+                var database = TestHelper.GetDatabaseFor(dbType, connection);
+
+                database.CreateTableIfNotExists<TestIgnore>();
+                database.Insert(new TestIgnore { Id = 1, FloatNumber = 1, Name = "test" });
+
+                var query = database.From<TestIgnore>().Where(t => t.Id == 1);
+
+                Assert.IsTrue(database.Exists(query));
+
+                connection.Close();
+            }
+        }
+
+        [Test, TestCaseSource(typeof(SqlTestDataFactory), nameof(SqlTestDataFactory.TestCases))]
+        public void ExistsReturnFalseWhenNotExists(TestHelper.DatabaseType dbType, string connectionString)
+        {
+            using (var connection = TestHelper.GetConnectionFor(dbType, connectionString))
+            {
+                var database = TestHelper.GetDatabaseFor(dbType, connection);
+
+                database.CreateTableIfNotExists<TestClass>();
+
+                var query = database.From<TestClass>().Where(t => t.Id == 1);
+
+                Assert.IsFalse(database.Exists(query));
+
+                connection.Close();
+            }
+        }
+
+        [Test, TestCaseSource(typeof(SqlTestDataFactory), nameof(SqlTestDataFactory.TestCases))]
+        public void CanSelectById(TestHelper.DatabaseType dbType, string connectionString)
+        {
+            using (var connection = TestHelper.GetConnectionFor(dbType, connectionString))
+            {
+                var database = TestHelper.GetDatabaseFor(dbType, connection);
+
+                database.CreateTableIfNotExists<TestClass>();
+                database.Insert(TestHelper.GetNewTestClass());
+
+                var obj = database.SelectById<TestClass>(1);
+
+                Assert.IsNotNull(obj);
+
+                connection.Close();
+            }
+        }
+    }
+}
